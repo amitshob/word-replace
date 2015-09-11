220a42c17ff192d516a62fb99656cefa8f635d6f
amitshob <amitshob@gmail.com>
2015-09-11 16:18:41 -0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -3,8 +3,8 @@
 var wordCount = function(temp,word) {
   // var occurence_count = (string_input.match(/word_to_count/g) || []).length;
   // toword_to_count = " " + word_to_count + " ";
-  var word_whole = " "+word+" ";
-  var count = (temp.match(word_whole) || []).length;
+  // var word_whole = " "+word+" ";
+  var count = (temp.match(/\bword_whole\g) || []).length;
   // debugger;
   return count;
 };
