cc91d6ccd262e23328430b894e6e299ab692db43
Amit <amitshob@gmail.com>
2015-09-11 11:07:34 -0700

    first spec

---
+++ spec/specs.js
@@ -0,0 +1,5 @@
+describe('wordReplace', function(){
+  it("should take a string input and give the number or occurences", function(){
+    expect(wordCount("Welcome to heaven").to.eql(1));
+  });
+});
